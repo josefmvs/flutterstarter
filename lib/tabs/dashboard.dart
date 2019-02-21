684307307c3b2910beca166f3ae8e2ed4d70a62d
Josef Solon <josef.solon@patienteer.co>
2019-02-21 23:02:41 +0800

    follow https://codelabs.developers.google.com/codelabs/first-flutter-app-pt1/#4

--- lib/tabs/dashboard.dart
+++ lib/tabs/dashboard.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) => new Container(
-    child: new Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        new Icon(
-          Icons.dashboard,
-          size: 150.0,
-          color: Colors.black12
-        ),
-        new Text('Dashboard tab content')
-      ]
-    ),
+    child: new RandomWords()
   );
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override                                  // Add from this line ...
+  Widget build(BuildContext context) {
+    final WordPair wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          //child: new Text(wordPair.asPascalCase), // Change this line to...
+          child: new RandomWords(),                 // ... this line.
+        ),
+      ),
+    );
+  }
 }
